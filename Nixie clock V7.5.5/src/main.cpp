c5bdc804f0fdb2e352692d048b3df7a12fcabea6
SkasLT <ljuskas.ternjej@gmail.com>
2021-10-14 14:28:06 +0200

    latest version of nixie clock project

--- Nixie clock V7.5.5/src/main.cpp
+++ Nixie clock V7.5.5/src/main.cpp
@@ -40,6 +40,11 @@ int hour1, hour2, minute1, minute2; //variables for storing each digit for hours
 
 RTC_DS3231 rtc;
 
+/**
+* Function for debouncing multiple buttons
+* @param buttonIndex the index of the button array to be read (button pins are storred in array)
+* @param debounceDelay time to delay between bounces (in milliseconds)
+*/
 bool debouncedButtonRead(int buttonIndex, const unsigned long debounceDelay)
 {
   // Read the state of the button pin into a local variable:
@@ -79,38 +84,45 @@ bool debouncedButtonRead(int buttonIndex, const unsigned long debounceDelay)
   return false;
 }
 
-//function for shifting out n bits into the storage register, with their defined values
+/**
+* function for shifting out n bits into the storage register, with their defined values
+* @param dataPin pin connected to serial data input for shift registers
+* @param clockPin clock pin connected to clock input for shift registers
+* @param bitOrder order of bits (LSB, MSB)
+* @param value sequential bit number to be shifted out
+* @param bits number of bits to shift out
+*/
 void shiftOutBits(int dataPin, int clockPin, int bitOrder, int value, int bits)
 {
-  for (int i = 0; i < bits; i++) // shift out n bits
+  for (int i = 0; i < bits; i++)
   {
-    if (bitOrder == LSBFIRST) //least significant bit first
+    if (bitOrder == LSBFIRST)
     {
-      digitalWrite(dataPin, (i == value)); //if increment value is same as needed value, set the dataPin HIGH
-      delayMicroseconds(10);               //delay for short time
+      digitalWrite(dataPin, (i == value));
+      delayMicroseconds(10);
     }
-    else //most significant bit first
+    else
     {
-      digitalWrite(dataPin, (bits - 1 - i) == value); //if increment value is same as needed, value set the dataPin HIGH
-      delayMicroseconds(10);                          //delay for short time
+      digitalWrite(dataPin, (bits - 1 - i) == value);
+      delayMicroseconds(10);
     }
-    digitalWrite(clockPin, HIGH); //pulse the clock pin in order to store data in storage register
+    digitalWrite(clockPin, HIGH);
     digitalWrite(clockPin, LOW);
   }
 }
 
-//this function updates displayed time
+//This function updates displayed time
 void updateDisplayedTime()
 {
-  digitalWrite(latchPin, LOW);                            //set the latch pin LOW when shifting data to the storage register
-  shiftOutBits(dataPin, clockPin, LSBFIRST, minute2, 10); //shift out minute2 value
-  shiftOutBits(dataPin, clockPin, LSBFIRST, minute1, 10); //shift out minute1 value
-  shiftOutBits(dataPin, clockPin, LSBFIRST, hour2, 10);   //shift out hour2 value
-  shiftOutBits(dataPin, clockPin, LSBFIRST, hour1, 10);   //shift out hour1 value
-  digitalWrite(latchPin, HIGH);                           //set the latch pin HIGH in order to display data on shift register outputs
+  digitalWrite(latchPin, LOW);
+  shiftOutBits(dataPin, clockPin, LSBFIRST, minute2, 10);
+  shiftOutBits(dataPin, clockPin, LSBFIRST, minute1, 10);
+  shiftOutBits(dataPin, clockPin, LSBFIRST, hour2, 10);
+  shiftOutBits(dataPin, clockPin, LSBFIRST, hour1, 10);
+  digitalWrite(latchPin, HIGH);
 }
 
-//function for calculating first and last hour digit, first and last minute digit
+//Function for calculating first and last hour digit, first and last minute digit
 void calculateTime()
 {
   //separate first and second digit of hour value
@@ -122,30 +134,43 @@ void calculateTime()
   minute2 = minute % 10;
 }
 
-/*
-Function necessary for longevity of NIXIE tubes, it lights up every digit one after another and repeats it for a certain ammount of time, with frequency high enough for human eye not to notice.
-This should be done as frequently as possible, but every half hour will do just fine
+/**
+* Function necessary for longevity of NIXIE tubes, it lights up every digit one after another and repeats it for a certain ammount of time, with frequency high enough for human eye not to notice.
+* This should be done as frequently as possible, but every half hour will do just fine
+* @param timeInterval how long will cathode routine run (in milliseconds)
+* @param timeDelay time between digit changes (in milliseconds)
 */
-void doCathodeRoutine(const unsigned long timeInterval) //timeInterval --> how long will cathode routine run (in milliseconds)
+void doCathodeRoutine(const unsigned long timeInterval, const unsigned long timeDelay)
 {
-  unsigned long previousTime = millis(); //local variable for storing the starting time of cathode routine interval
+  unsigned long previousTime = millis();
 
-  while ((millis() - previousTime) <= timeInterval) //do cathode routine until some time has passed (timeInterval)
+  while ((millis() - previousTime) <= timeInterval)
   {
     for (int i = 0; i < 10; i++) //loop runs 10 times since every NIXIE tube has 10 digits (0...9)
     {
-      digitalWrite(latchPin, LOW);                      //set the latch pin LOW when shifting data to the storage register
-      shiftOutBits(dataPin, clockPin, LSBFIRST, i, 10); //shifts out 10 bits while lighting up the corresponding digit once; in this case minute2 digit
-      shiftOutBits(dataPin, clockPin, LSBFIRST, i, 10); //shifts out 10 bits while lighting up the corresponding digit once; in this case minute1 digit
-      shiftOutBits(dataPin, clockPin, LSBFIRST, i, 10); //shifts out 10 bits while lighting up the corresponding digit once; in this case hour2 digit
-      shiftOutBits(dataPin, clockPin, LSBFIRST, i, 10); //shifts out 10 bits while lighting up the corresponding digit once; in this case hour1 digit
-      digitalWrite(latchPin, HIGH);                     //set the latch pin HIGH in order to display data on shift register outputs
+      digitalWrite(latchPin, LOW);
+      shiftOutBits(dataPin, clockPin, LSBFIRST, i, 10);
+      shiftOutBits(dataPin, clockPin, LSBFIRST, i, 10);
+      shiftOutBits(dataPin, clockPin, LSBFIRST, i, 10);
+      shiftOutBits(dataPin, clockPin, LSBFIRST, i, 10);
+      digitalWrite(latchPin, HIGH);
+      delay(timeDelay);
+    }
+    for (int i = 8; i > 0; i--) //loop runs 10 times since every NIXIE tube has 10 digits (9...0)
+    {
+      digitalWrite(latchPin, LOW);
+      shiftOutBits(dataPin, clockPin, LSBFIRST, i, 10);
+      shiftOutBits(dataPin, clockPin, LSBFIRST, i, 10);
+      shiftOutBits(dataPin, clockPin, LSBFIRST, i, 10);
+      shiftOutBits(dataPin, clockPin, LSBFIRST, i, 10);
+      digitalWrite(latchPin, HIGH);
+      delay(timeDelay);
     }
   }
   debugln("cathode routine has completed");
 }
 
-//function that calculates current minutes, hours, seconds and their respective digits
+//unction that calculates current minutes, hours, seconds and their respective digits
 void getCurrentTime()
 {
   DateTime now = rtc.now();
@@ -172,16 +197,16 @@ void firstMenuPage()
 {
   while (setupMode == 1)
   {
-    digitalWrite(hourLed, HIGH);    //turn on hourLed when setting up hours
-    updateDisplayedTime();          //evey time hour value is changed update the displayed time
-    if (debouncedButtonRead(0, 50)) //if setup button is pressed go to the next menu page
+    digitalWrite(hourLed, HIGH);
+    updateDisplayedTime();
+    if (debouncedButtonRead(0, 50))
       setupMode++;
-    if (debouncedButtonRead(1, 50)) //increment hour value when increment button is pressed
-      hour = (hour + 1) % 24;       //make sure that hour value overflows when it reaches 24 and it goes back to 0
-    calculateTime();                //calculate hour and minute digit values:
-    debug("Set hours : ");          //print out current menu page
-    debugln(hour);                  //print out hour value on serial monitor
-    digitalWrite(hourLed, LOW);     //turn off hourLed when done with setting up hours
+    if (debouncedButtonRead(1, 50))
+      hour = (hour + 1) % 24;
+    calculateTime();
+    debug("Set hours : ");
+    debugln(hour);
+    digitalWrite(hourLed, LOW);
   }
 }
 
@@ -190,24 +215,46 @@ void secondMenuPage()
 {
   while (setupMode == 2)
   {
-    digitalWrite(minuteLed, HIGH);  //turn on minuteLed when setting up minutes
-    updateDisplayedTime();          //evey time minute value is changed update the displayed time
-    if (debouncedButtonRead(0, 50)) //if setup button is pressed go to the next menu page
+    digitalWrite(minuteLed, HIGH);
+    updateDisplayedTime();
+    if (debouncedButtonRead(0, 50))
       setupMode++;
-    if (debouncedButtonRead(1, 50)) //increment minute value when increment button is pressed
-      minute = (minute + 1) % 60;   //make sure that minute value overflows when it reaches 60 and it goes back to 0
-    calculateTime();                //calculate hour and minute digit values:
-    debug("Set minutes : ");        //print out current menu page
-    debugln(minute);                //print out hour value on serial monitor
-    digitalWrite(minuteLed, LOW);   //turn off minuteLed when done setting up minutes
+    if (debouncedButtonRead(1, 50))
+      minute = (minute + 1) % 60;
+    calculateTime();
+    debug("Set minutes : ");
+    debugln(minute);
+    digitalWrite(minuteLed, LOW);
   }
 }
 
 //last menu page that sets adjusted time in the RTC module
 void lastMenuPage()
 {
-  rtc.adjust(DateTime(rtc.now().year(), rtc.now().month(), rtc.now().day(), hour, minute, 0)); //set adjusted time in RTC module
-  setupMode = 0;                                                                               //set setupMode to 0; exits menu
+  rtc.adjust(DateTime(rtc.now().year(), rtc.now().month(), rtc.now().day(), hour, minute, 0));
+  setupMode = 0;
+}
+
+/**
+* check if minute value has changed, and if it did, update displayed time
+* @param timeToPass cathodeRoutine will run every timeToPass (in minutes)
+*/
+void timeChange(int timeToPass)
+{
+  if (minuteChange != minute)
+  {
+    updateDisplayedTime();
+    minuteChange = minute;
+    minuteCounter++;
+
+    //check if half an hour has passed and do CathodeRoutine
+    if (minuteCounter == timeToPass)
+    {
+      debugln("Half an hour has passed, doing cathodeRoutine...");
+      doCathodeRoutine(3000, 25);
+      minuteCounter = 0;
+    }
+  }
 }
 
 void setup()
@@ -216,24 +263,21 @@ void setup()
   while (!rtc.begin())
     continue;
 
-  //Since we can have alot of button pins, it is most effective to use a for loop that sets the pinMode of button pins.
   for (int i = 0; i < number_of_buttons; i++)
     pinMode(button[i], INPUT_PULLUP);
 
-  pinMode(latchPin, OUTPUT);    //set latch pin as output
-  pinMode(clockPin, OUTPUT);    //set clock pin as output
-  pinMode(dataPin, OUTPUT);     //set data pin as output
-  pinMode(masterReset, OUTPUT); //set master reset pin as output
-  pinMode(hourLed, OUTPUT);     //set hourLed pin as output
-  pinMode(minuteLed, OUTPUT);   //set minuteLed pin as output
+  pinMode(latchPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);
+  pinMode(masterReset, OUTPUT);
+  pinMode(hourLed, OUTPUT);
+  pinMode(minuteLed, OUTPUT);
 
-  //before proceeding we will reset all shift registers to make sure that there are no undefined previous states on their data inputs (all shift register reset pints are tied together)
   digitalWrite(masterReset, LOW);
   delayMicroseconds(10);
   digitalWrite(masterReset, HIGH);
-  doCathodeRoutine(5000); //when powering the clock for the first time do cathode routine
+  doCathodeRoutine(2000, 25);
 
-  //establish serial communication with baud rate of 9600
   debug_begin(9600);
 }
 
@@ -241,35 +285,21 @@ void loop()
 {
   //get current time
   getCurrentTime();
+  //check for time change
+  timeChange(15);
 
   //constantly check for setupButton state
   if (debouncedButtonRead(0, 50))
     setupMode++;
 
-  switch (setupMode) //menu for changing hours and minutes, can be expanded by adding aditional cases
+  switch (setupMode)
   {
   case 1:
-    firstMenuPage(); //menu page for changing hours
+    firstMenuPage();
   case 2:
-    secondMenuPage(); //menu page for changing minutes
+    secondMenuPage();
   case 3:
-    lastMenuPage(); //last menu page that sets adjusted time in the RTC module
+    lastMenuPage();
     break;
-  }
-
-  //chech if minute value has changed, and if it did, update displayed time
-  if (minuteChange != minute)
-  {
-    updateDisplayedTime();
-    minuteChange = minute; //save the current minute value
-    minuteCounter++;       //add to minuteCounter to keep track of time (half hour)
-
-    //check if half an hour has passed and do CathodeRoutine
-    if (minuteCounter == 30)
-    {
-      debugln("Half an hour has passed, doing cathodeRoutine...");
-      doCathodeRoutine(5000);
-      minuteCounter = 0; //reset minuteCounter until next half an hour has passed
-    }
   }
 }
